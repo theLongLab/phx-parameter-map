eace49e916ceab9e70c4f1d92496fcd36bdd8eb2
Michael Li <minghao.li1@ucalgary.ca>
2019-12-17 14:12:55 -0700

    Fix minor errors and move data cleaning
    make_dataset.py
    - File paths and newlines in file writing now properly implemented.
    
    build_features.py
    - Data cleaning moved here.
    - New target variable is MCC/JSD ratio.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,13 +7,22 @@ import sys
 
 def main(phx_project_dpath: Path, phx_output_dpaths_fname: str) -> None:
     """
+    Create list of PoolHapX output directories from given PoolHapX project path.
+
+    Parameters
+    ----------
+    phx_project_dpath : pathlib.Path
+        PoolHapX project directory.
+
+    phx_output_dpaths_fname : str
+        File path for text file containing PoolHapX output directory paths.
     """
     phx_output_dpaths: Path = Path(
-        Path(__file__).cwd().parents[1], "data", "raw", phx_output_dpaths_fname
+        Path(__file__).absolute().parents[2], "data", "raw", phx_output_dpaths_fname
     )
     run(
         "cp {} {}".format(
-            Path(phx_project_dpath, "param_sim", "phx_params.txt"), phx_output_dpaths.cwd()
+            Path(phx_project_dpath, "param_sim", "phx_params.txt"), phx_output_dpaths.parent
         ),
         shell=True,
     )
@@ -22,11 +31,11 @@ def main(phx_project_dpath: Path, phx_output_dpaths_fname: str) -> None:
         directory: Path
         for directory in Path(phx_project_dpath, "output").iterdir():
             if directory.is_dir():
-                output.write(str(directory.absolute()))
+                output.write(str(directory.absolute()) + "\n")
 
 
 if __name__ == "__main__":
-    phx_project_dpath: Path = Path(sys.argv[1])
+    phx_project_dpath: Path = Path(sys.argv[1]).absolute()
     phx_output_dpaths_fname: str = sys.argv[2]
 
-    main(phx_project_dpath, phx_output_dpaths_fname)
+    main(phx_project_dpath=phx_project_dpath, phx_output_dpaths_fname=phx_output_dpaths_fname)
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,11 +1,11 @@
 # src/features/build_features.py
 
-import csv
 from pathlib import Path
-import sys
 from subprocess import run
+import sys
 
 import pandas as pd
+from pandas.core.indexes.range import RangeIndex
 
 
 def _loop_dirs(dir_path: pd.Series, compiled_metrics: pd.DataFrame) -> None:
@@ -23,13 +23,17 @@ def _loop_dirs(dir_path: pd.Series, compiled_metrics: pd.DataFrame) -> None:
     idx: str = dir_path.name
     sim: str = dir_path[0].split("/")[-1]
 
-    mcc: str = run(
-        "tail -1 {}/MCC.result".format(dir_path[0]), shell=True, capture_output=True
-    ).stdout.decode()[0:-1].split("\t")[1]
+    # Exception handling for failed PoolHapX runs.
+    try:
+        mcc: str = run(
+            "tail -1 {}/MCC.result".format(dir_path[0]), shell=True, capture_output=True
+        ).stdout.decode()[0:-1].split("\t")[1]
 
-    jsd: str = run(
-        "tail -1 {}/JSD.result".format(dir_path[0]), shell=True, capture_output=True
-    ).stdout.decode()[0:-1].split("\t")[1]
+        jsd: str = run(
+            "tail -1 {}/JSD.result".format(dir_path[0]), shell=True, capture_output=True
+        ).stdout.decode()[0:-1].split("\t")[1]
+    except IndexError:
+        mcc = jsd = "NaN"
 
     compiled_metrics.loc[idx] = [sim, float(mcc), float(jsd)]  # add new row
 
@@ -56,14 +60,19 @@ def _compile_metrics(output_dirs: pd.DataFrame, project_data_dir: Path) -> pd.Da
     unused: pd.DataFrame = output_dirs.apply(_loop_dirs, axis=1, compiled_metrics=compiled_metrics)
     del unused  # delete unused pandas apply stdout
 
-    pd.to_csv(str(Path(project_data_dir, "interim", "phx_compiled_metrics.csv")), index=False)
+    # Sort and write to interim directory.
+    compiled_metrics.sort_values(by="sim", inplace=True)
+    compiled_metrics.to_csv(
+        str(Path(project_data_dir, "interim", "phx_compiled_metrics.csv")), index=False
+    )
+
     return compiled_metrics
 
 
 def _process_metrics(compiled_metrics: pd.DataFrame, project_data_dir: Path) -> None:
     """
-    Given a dataframe containing all compiled MCC and JSD metrics, subtract JSD from MCC and write
-    to a CSV file in data/processed.
+    Given a dataframe containing all compiled MCC and JSD metrics, perform data cleaning, divide
+    JSD from MCC, and write to a CSV file in data/processed.
 
     Parameters
     ----------
@@ -73,14 +82,32 @@ def _process_metrics(compiled_metrics: pd.DataFrame, project_data_dir: Path) -> 
     project_data_dir : pathlib.Path
         The directory for the data directory in this Cookiecutter project.
     """
-    processed_metrics: pd.DataFrame = compiled_metrics
-    processed_metrics["mcc_minus_jsd"] = processed_metrics["mcc"] - processed_metrics["jsd"]
+    # Load raw PHX parameters.
+    processed_phx_params: pd.DataFrame = pd.read_csv(
+        Path(project_data_dir, "raw", "phx_params.txt"), sep="\t"
+    ).drop("Regression_Gamma_Max", axis=1)
+
+    # Drop rows with negative MCC values(?) and NaN values by index.
+    drop: RangeIndex = compiled_metrics[
+        (compiled_metrics["mcc"] < 0) | (compiled_metrics["jsd"].isnull())
+    ].index
+    processed_metrics: pd.DataFrame = compiled_metrics.drop(drop)
+    processed_phx_params.drop(drop, inplace=True)
+
+    # Create MCC/JSD ratio column and drop individual columns.
+    processed_metrics["mcc_jsd_ratio"] = processed_metrics["mcc"] / processed_metrics["jsd"]
     processed_metrics.drop(["mcc", "jsd"], axis=1, inplace=True)
 
-    pd.to_csv(str(Path(project_data_dir, "processed", "phx_processed_metrics.csv")), index=False)
+    # Write processed X and y tables to file.
+    processed_phx_params.to_csv(
+        str(Path(project_data_dir, "processed", "phx_processed_params.csv")), index=False
+    )
+    processed_metrics.to_csv(
+        str(Path(project_data_dir, "processed", "phx_processed_metrics.csv")), index=False
+    )
 
 
-def main(phx_output_dpaths_file: Path) -> None:
+def main(phx_output_dpaths_fpath: Path) -> None:
     """
     Given a text file containing all simulation output directory paths, compile and process MCC and
     JSD metrics and store in data/interm and data/processed.
@@ -90,15 +117,15 @@ def main(phx_output_dpaths_file: Path) -> None:
     output_dir_paths_file : pathlib.Path
         Text file containing paths to each simulation output directory.
     """
-    phx_output_dpaths: pd.DataFrame = pd.read_csv(phx_output_dpaths_file, header=None)
-    project_data_dpath: Path = phx_output_dpaths_file.cwd().parent
+    phx_output_dpaths: pd.DataFrame = pd.read_csv(phx_output_dpaths_fpath, header=None)
+    project_data_dpath: Path = phx_output_dpaths_fpath.parents[1]
     compiled_metrics: pd.DataFrame = _compile_metrics(phx_output_dpaths, project_data_dpath)
     _process_metrics(compiled_metrics, project_data_dpath)  # save in data/processed/
 
 
 if __name__ == "__main__":
-    phx_output_dpaths_filename: Path = Path(
-        Path(__file__).cwd().parents[1], "data", "raw", sys.argv[1]  # in data/raw/
+    phx_output_dpaths_fpath: Path = Path(
+        Path(__file__).absolute().parents[2], "data", "raw", sys.argv[1]  # in data/raw/
     )
 
-    main(phx_output_dpaths_filename)
+    main(phx_output_dpaths_fpath)
